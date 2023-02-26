fbce811fc3d80f7cab91034568c12b795c6fe6c4
Jason Ferguson <fergusonjason@gmail.com>
2023-02-26 06:53:00 -0500

    add cookie to response

--- data/src/main/java/org/hiredgoons/myapp/data/controller/HelloWorldController.java
+++ data/src/main/java/org/hiredgoons/myapp/data/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package org.hiredgoons.myapp.data.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/helloWorld")
-    public String helloWorld() {
+    public String helloWorld(HttpServletResponse response) {
+
+        // c is for cookie and that's good enough for me
+        Cookie c = new Cookie("TEST","TEST");
+        response.addCookie(c);
+
         return "Hello world";
     }
 }
